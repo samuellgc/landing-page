97c22c348f8dbf2278d3a3b85ef12a69f7251533
samuellgc <samuellgc@hotmail.com>
2022-01-27 19:29:05 -0300

    Criação do arquivo data para salvar os dados referentes aos templates. Melhor configuração e ajustes do template.ejs e do index.js

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 var express = require('express');
 var app = express();
 const path = require('path')
+const data = require('./data.json')
 
 app.set('view engine', 'ejs')
 
 app.use(express.static("public"))
 
 app.set('views', path.join(__dirname, 'views'))
-
-const data =  {
-    titulo: "CONSULTOR DE NEGÓCIOS - VENDAS PUBLICIDADE",
-    requisitos: "Graduação em Marketing ou Publicidade e Propaganda. Experiência com Vendas. Conhecimento de gestão de indicadores, negociação e informática.",
-    atividades: "Planejamento de vendas, visita a clientes, desenvolvimento de propostas comerciais e plano de mídias, acompanhamento de indicadorese elaboração de orçamentos.",
-    perfil: "Comunicativo, negociador, ágil e responsável.",
-    horario: "8 - 18h"
-}
 
 app.get('/', function(req, res) {
     res.render('../views/template.ejs', {...data});
